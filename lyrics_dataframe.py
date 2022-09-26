888fc78f97573416d0340f8751ea54abf2d52b8c
AlmirFonseca <almirfonseca60@gmail.com>
2022-09-26 19:53:10 -0300

    Crie a função 'get_artist_info' utilizando a API da Genius

--- lyrics_dataframe.py
+++ lyrics_dataframe.py
@@ -11,3 +11,14 @@ access_token = "u2SqMOrCtzWwY9xGxI6PiLn5aVqnhzWMiaMWB2BmrfuvJQL-Z_nQ4pv8gJej4isU
 
 # Instancia o objeto principal da API do Genius
 genius = Genius(access_token, timeout=60, retries=10)
+
+# Extrai os dados do artista através do seu nome
+def get_artist_info(artist_name):
+    # Realiza uma busca pelo nome do artista
+    artist = genius.search_artist(artist_name, max_songs=0, get_full_info=False)
+    
+    # Retorna o nome e o id do artista na plataforma Genius
+    return artist.name, artist.id
+
+# Obtém o nome e o id do artista
+artist_name, artist_id = get_artist_info("Coldplay")
