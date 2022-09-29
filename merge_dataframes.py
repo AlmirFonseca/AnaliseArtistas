7c6d677083ce25f9f9842ec2652d4b3d83ca619e
AlmirFonseca <almirfonseca60@gmail.com>
2022-09-29 18:47:21 -0300

    Atualize a função de filtro para receber e retornar pd.DataFrame

--- merge_dataframes.py
+++ merge_dataframes.py
@@ -1,23 +1,29 @@
 import numpy as np
 import pandas as pd
 
-def filter_albums(albums, filter_terms, case_sensitive=False, reverse=False):
+# TODO Remover "duplicatas"
+# TODO Ordem alfabética
+# TODO Número de tracks
+# TODO 
+
+def filter_albums(dataframe, dataframe_column, filter_terms, case_sensitive=False, reverse=False):
     filter_terms_list = filter_terms.split(",")
     for term_index, term in enumerate(filter_terms_list):
         filter_terms_list[term_index] = term.strip()
     
-    mask = np.zeros(albums.shape[0], dtype=bool)
+    mask = np.zeros(dataframe.shape[0], dtype=bool)
     
     for term in filter_terms_list:
-        mask = mask | albums.str.contains(term, case=case_sensitive)
+        mask = mask | dataframe[dataframe_column].str.contains(term, case=case_sensitive)
         
     if reverse:
         mask = ~mask
     
-    return albums[~mask]
+    return dataframe[~mask]
 
 df = pd.read_csv("Letras - Coldplay.csv", encoding="ISO-8859-1", sep=";")
 
-df_filter = filter_albums(df["album_name"], "  live,remix ,edition,radio,session, version")
+df_filter = filter_albums(df, "album_name", "  live,remix ,edition, deluxe,radio,session, version")
 
-print(df_filter.unique())
+# print(df_filter.unique())
+print(df_filter)
