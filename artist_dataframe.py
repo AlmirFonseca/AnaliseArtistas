d1552092920d5050da4a378f588952b22e5d7427
Lavínia Dias <103752863+LaviniaSD@users.noreply.github.com>
2022-10-01 08:34:58 -0300

    Corrija bug
    Corrija bug na função discography e tracks info

--- artist_dataframe.py
+++ artist_dataframe.py
@@ -43,58 +43,56 @@ def contribuitors(track):
         track_contributors_str = "/".join(track_contributors_list)#Transforma a lista em uma string
     return track_contributors_str
     
-# Recebe uma faixa e retorna a string "Sim" se ela for explícita e "Não" se não for
+# Recebe uma faixa e retorna a string "Yes" se ela for explícita e "No" se não for
 def is_explicit(track):
     track_explicit_lyrics=track.explicit_lyrics
     if track_explicit_lyrics == True:
-        track_explicit_lyrics_str = "Sim"
+        track_explicit_lyrics_str = "Yes"
     else:
-        track_explicit_lyrics_str = "Não"
+        track_explicit_lyrics_str = "No"
     return track_explicit_lyrics_str
- 
-# Recebe faixas e retorna os dados de cada uma extraídos da plataforma Deezer
-def tracks_info(album_tracks):
-    for track in album_tracks:
-        track_title = track.title
-        track_duration = time.strftime("%M:%S", time.gmtime(track.duration))
-        track_position = track.track_position
-        track_disk_number = track.disk_number
-        track_explicit_lyrics = is_explicit(track) #Utiliza a função de apoio is_explicit
-        track_gain = track.gain
-        track_contributors = contribuitors(track) #Utiliza a função de apoio contribuitors
+
+# Recebe uma faixa e retorna os dados de cada uma extraídos da plataforma Deezer
+def track_info(track):
+    track_title = track.title
+    track_duration = time.strftime("%M:%S", time.gmtime(track.duration))
+    track_position = track.track_position
+    track_disk_number = track.disk_number
+    track_explicit_lyrics = is_explicit(track) #Utiliza a função de apoio is_explicit
+    track_gain = track.gain
+    track_contributors = contribuitors(track) #Utiliza a função de apoio contribuitors
     return track_title,track_duration,track_position,track_disk_number,track_explicit_lyrics,track_gain,track_contributors
 
 #Utiliza as outras funções para criar um dataframe com informações da discografia do artista
 def discography(artist):
     try:
         #Cria um dicionário onde serão armazenadas as informações sobre a discografia do artista
-        song_data = {"Álbum": [], 
-                     "Gênero": [], 
-                     "Data de lançamento": [], 
-                     "Número da faixa": [], 
-                     "Título da faixa": [], 
-                     "Contribuidores": [], 
-                     "Duração": [], 
-                     "Letra Explícita": [], 
-                     "Ganho": []}
-        artist_info(artist) # Busca a informações do artista
+        song_data = {"Album": [], 
+                     "Genre": [], 
+                     "Release Date": [], 
+                     "Track Number": [], 
+                     "Track": [], 
+                     "Contribuitors": [], 
+                     "Duration": [], 
+                     "Explicit": [], 
+                     "Gain": []}
         albums = artist_info(artist)[3]# Busca os álbuns do artista
         for album in albums:
             album_data= album_info(album) #Busca a informação de cada álbum
             tracks = album_data[5] # Busca as faixas em um álbum
             for track in tracks:
-                track_data = tracks_info(track) #Busca a informação de cada faixa
-                #Adiciona no dicionário dong_data as informações de cada faixa
-                song_data.get("Álbum").append(album_data[0])
-                song_data.get("Gênero").append(album_data[1])
-                song_data.get("Data de lançamento").append(album_data[4])
-                song_data.get("Número da faixa").append(track_data [2])
-                song_data.get("Título da faixa").append(track_data [0])
-                song_data.get("Contribuidores").append(track_data [6])
-                song_data.get("Duração").append(track_data [1])
-                song_data.get("Letra Explícita").append(track_data [4])
-                song_data.get("Ganho").append(track_data [5])
-        df_discografia = pd.DataFrame.from_dict(song_data) # Cria um dataframe a partir do dicionário
+                track_data = track_info(track) #Busca a informação de cada faixa
+                #Adiciona no dicionário song_data as informações de cada faixa
+                song_data.get("Album").append(album_data[0])
+                song_data.get("Genre").append(album_data[1])
+                song_data.get("Release Date").append(album_data[4])
+                song_data.get("Track Number").append(track_data [2])
+                song_data.get("Track").append(track_data[0])
+                song_data.get("Contribuitors").append(track_data [6])
+                song_data.get("Duration").append(track_data [1])
+                song_data.get("Explicit").append(track_data [4])
+                song_data.get("Gain").append(track_data [5])
+                df_discografia = pd.DataFrame.from_dict(song_data) # Cria um dataframe a partir do dicionário
     except TypeError as te: #Avisa ao usuário caso a função não receba uma string
         print("Essa função deve receber uma string;",te)
     except IndexError as ie: #Avisa ao usuário caso o artista procurado não seja encontrado
@@ -102,8 +100,5 @@ def discography(artist):
     else:
         df_discografia.to_csv("discografia.csv", sep=";", encoding="utf-8-sig", index=False) #Converte o dataframe para um arquivo csv
         print("Arquivo Criado com sucesso")
-
-
-
 
 discography("Coldplay")
