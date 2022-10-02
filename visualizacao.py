3afc4b5becb61ade2064c14db614da49a3035023
Lavínia Dias <103752863+LaviniaSD@users.noreply.github.com>
2022-10-02 14:58:54 -0300

    Mude o nome das colunas dos dataframes

--- visualizacao.py
+++ visualizacao.py
@@ -11,25 +11,26 @@ sns.set_theme()
 
 def most_listened_plot(dataframe):
     data = ae.most_listened(dataframe)
-    sns.barplot(data=data, x="Popularidade", y=data.index.get_level_values(1))
+    sns.barplot(data=data, x="Popularity", y=data.index.get_level_values(1))
     plt.show()
-
+    
+    
 def least_listened_plot(dataframe):
     data = ae.least_listened(dataframe)
-    sns.barplot(data=data, x="Popularidade", y=data.index.get_level_values(1))
+    sns.barplot(data=data, x="Popularity", y=data.index.get_level_values(1))
     plt.show()
     
 def longest_plot(dataframe):
     data = ae.longest(dataframe)
-    sns.barplot(data=data, x="Duração", y=data.index.get_level_values(1))
-    plt.show()  
+    sns.barplot(data=data, x="Duration", y=data.index.get_level_values(1))
+    plt.show() 
     
 def shortest_plot(dataframe):
     data = ae.shortest(dataframe)
-    sns.barplot(data=data, x="Duração", y=data.index.get_level_values(1))
+    sns.barplot(data=data, x="Duration", y=data.index.get_level_values(1))
     plt.show()
 
-    
+  
 def common_words_by_lyrics_plot(dataframe):
     resultado = ae.common_words_by_lyrics(dataframe)
     frequency = {}
