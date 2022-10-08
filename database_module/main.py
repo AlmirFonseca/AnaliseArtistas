8a0fb1138b19505f268cd0ae2c420d3e99c3ee2b
AlmirFonseca <almirfonseca60@gmail.com>
2022-10-08 20:35:22 -0300

    Trate exceções na 'main.py' e incremente a exibição de logs

--- database_module/main.py
+++ database_module/main.py
@@ -10,5 +10,22 @@ ge_access_token = "u2SqMOrCtzWwY9xGxI6PiLn5aVqnhzWMiaMWB2BmrfuvJQL-Z_nQ4pv8gJej4
 # Define o nome do artista a ser analisado
 artist_name = "Coldplay"
 
-# Gera e salva num arquivo ".csv" um dataframe contendo os dados do artista, seus álbuns e suas faixas
-coldplay_data = adg.generate_dataframe_of(artist_name, sp_client_id, sp_client_secret, ge_access_token, filter_terms="", save_csv=True)
+# Imprime uma mensagem inicial
+print("\u001b[32;1m{}\033[m".format(f"\nIniciando a geração de base de dados do {artist_name}..."))
+
+try:
+    # Gera e salva num arquivo ".csv" um dataframe contendo os dados do artista, seus álbuns e suas faixas
+    coldplay_data = adg.generate_dataframe_of(artist_name, sp_client_id, sp_client_secret, ge_access_token, filter_terms="", save_csv=True)
+    
+# Recebe as exceções e as exibe de maneira amigável ao usuário 
+except BaseException as error:
+    print("\u001b[34;1m{}\033[m".format("=-"*60))
+    print("Ocorreu um erro durante o execução do projeto:\n", error.__class__.__name__, "\n", error)
+# Caso nenhuma exceção ocorra durante a execução
+else:
+    print("\u001b[34;1m{}\033[m".format("=-"*60))
+    print("\u001b[32;1m{}\033[m".format("A execução foi concluída com sucesso!"))
+# Exibe uma mensagem ao final da execução
+finally:
+    print("\u001b[34;1m{}\033[m".format("=-"*60))
+    print("\nEncerrando a aplicação...")
